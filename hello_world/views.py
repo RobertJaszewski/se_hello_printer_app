19413ed744fc98fa6f8b753b696028720aaca123
RobertJaszewski <RobertJaszewski@users.noreply.github.com>
2022-05-28 14:31:23 +0200

    File views.py added

---
+++ hello_world/views.py
@@ -0,0 +1,19 @@
+from hello_world import app
+from hello_world.formatter import get_formatted
+from hello_world.formatter import SUPPORTED, PLAIN
+from flask import request
+
+moje imie = "Robert"
+msg = "Hello World!"
+
+app.route('/')
+def index():
+    output = request.args.get('output')
+    if not output:
+        output = PLAIN
+    return get_formatted(msg, moje_imie,
+                         output.lower())
+
+@app.route('/outputs')
+def supported_output():
+    return ", ".join(SUPPORTED)
